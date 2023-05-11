fa19227c5b974eafcf55a66280a85e4f5cf60b81
vkohutid <vkohut@intelliarts.com>
2023-05-11 11:42:33 +0300

    Initial Commit

---
+++ ExploreHub/ExploreHubApp.swift
@@ -0,0 +1,20 @@
+//
+//  ExploreHubApp.swift
+//  ExploreHub
+//
+//  Created by Vitalii Kohut on 11.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ExploreHubApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
